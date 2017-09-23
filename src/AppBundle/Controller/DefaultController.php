b5d625bf19d867ff6beb6f1be2f368e80fe2ba3f
Sorrow <sanjuan.thibaut@gmail.com>
2017-09-23 20:08:04 +0200

    init route and view

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->redirectToRoute('accueil');
+    }
+
+    /**
+     * @Route("/accueil", name="accueil")
+     * @Method("GET")
+     */
+    public function accueilAction()
+    {
+        return $this->render('@App/Default/accueil.html.twig', array(
+            'img' => 'assets/img/accueil.jpg',
+            'position' => 'bottom'
+        ));
+    }
+
+    /**
+     * @Route("body-karate", name="body-karate")
+     * @Method("GET")
+     */
+    public function bodyKarateAction()
+    {
+        return $this->render('@App/Default/body-karate.html.twig', array(
+            'img' => 'assets/img/body-karate.jpg',
+            'position' => 'top'
+        ));
+    }
+
+    /**
+     * @Route("coaching", name="coaching")
+     * @Method("GET")
+     */
+    public function coachingAction()
+    {
+        return $this->render('@App/Default/coaching.html.twig', array(
+            'img' => 'assets/img/coaching.jpg',
+            'position' => 'bottom'
+        ));
+    }
+
+    /**
+     * @Route("club", name="club")
+     * @Method("GET")
+     */
+    public function clubAction()
+    {
+        return $this->render('@App/Default/club.html.twig', array(
+            'img' => 'assets/img/club.jpg',
+            'position' => 'bottom'
+        ));
+    }
+
+    /**
+     * @Route("boutique", name="boutique")
+     * @Method("GET")
+     */
+    public function boutiqueAction()
+    {
+        return $this->render('@App/Default/boutique.html.twig', array(
+            'img' => 'assets/img/boutique.jpg',
+            'position' => 'center'
+        ));
+    }
+
+    /**
+     * @Route("contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('@App/Default/contact.html.twig', array(
+            'img' => 'assets/img/contact.jpg',
+            'position' => 'center'
+        ));
+    }
+}
---
+++ src/AppBundle/Controller/KarateController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/karate-contact")
+ */
+class KarateController extends Controller
+{
+    /**
+     * @Route("/jeune-enfant", name="kc.jeune-enfant")
+     * @Method("GET")
+     */
+    public function jeuneEnfantAction()
+    {
+        return $this->render('@App/KarateContact/jeune-enfant.html.twig', array(
+            'img' => 'assets/img/jeune-enfant.jpg',
+            'position' => 'center'
+        ));
+    }
+
+    /**
+     * @Route("/enfant", name="kc.enfant")
+     * @Method("GET")
+     */
+    public function enfantAction()
+    {
+        return $this->render('@App/KarateContact/enfant.html.twig', array(
+            'img' => 'assets/img/enfant.jpg',
+            'position' => 'top'
+        ));
+    }
+
+    /**
+     * @Route("/ado-adulte", name="kc.ado-adulte")
+     * @Method("GET")
+     */
+    public function adoAdulteAction()
+    {
+        return $this->render('@App/KarateContact/ado-adulte.html.twig', array(
+            'img' => 'assets/img/ado-adulte.jpg',
+            'position' => 'top'
+        ));
+    }
+}
