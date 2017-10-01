1efc129b5dd3a562d478cfd457aa95b00e93a901
Sorrow <sanjuan.thibaut@gmail.com>
2017-10-01 16:45:51 +0200

    Terminate PostController

--- src/AdminBundle/Controller/PostController.php
+++ src/AdminBundle/Controller/PostController.php
@@ -19,7 +19,12 @@ class PostController extends Controller
      */
     public function indexAction()
     {
-        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+        // SELECT * FROM posts;
+        $posts = $this
+            ->getDoctrine()
+            ->getRepository(Post::class)
+            ->findAll()
+        ;
 
         return $this->render('@Admin/Post/index.html.twig', array(
             'posts' => $posts
@@ -27,7 +32,7 @@ class PostController extends Controller
     }
 
     /**
-     * @Route("/add", name="admin.post.add")
+     * @Route("/post/add", name="admin.post.add")
      */
     public function addAction(Request $request)
     {
@@ -51,18 +56,38 @@ class PostController extends Controller
     }
 
     /**
-     * @Route("/edit", name="admin.post.edit")
+     * @Route("/post/edit/{id}", name="admin.post.edit")
+     *
+     * @param Post $post
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function editAction()
+    public function editAction(Post $post, Request $request)
     {
-        return $this->render('@Admin/Post/index.html.twig');
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin.posts');
+        }
+
+        return $this->render('@Admin/Post/form.html.twig', array(
+            'form' => $form->createView(),
+            'post' => $post
+        ));
     }
 
     /**
-     * @Route("/delete", name="admin.post.delete")
+     * @Route("/post/delete/{id}", name="admin.post.delete")
      */
-    public function deleteAction()
+    public function deleteAction(Post $post)
     {
-        return $this->render('@Admin/Post/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirectToRoute('admin.posts');
     }
 }
